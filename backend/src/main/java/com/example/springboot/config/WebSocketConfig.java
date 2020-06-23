540a5ba4d26083f335154322d7790fe99e2d23b7
Zhaolong Zhong <zhlgzh@gmail.com>
2020-06-23 09:23:55 -0700

    Added heart beat scheduler

--- backend/src/main/java/com/example/springboot/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/springboot/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.example.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -23,7 +26,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/chat", "/greeting");
+        config.enableSimpleBroker("/topic", "/chat", "/greeting")
+//                .setHeartbeatValue(new long[]{0, 1000})
+                .setTaskScheduler(heartBeatScheduler());
         config.setApplicationDestinationPrefixes("/topic");
+    }
+
+    @Bean
+    public TaskScheduler heartBeatScheduler() {
+        // https://stackoverflow.com/questions/39220647/spring-stomp-over-websockets-not-scheduling-heartbeats
+        return new ThreadPoolTaskScheduler();
     }
 }
